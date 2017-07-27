f875863ee20ee566b92402bee1eac248019c7209
Alex Lee <alex@uniteus.com>
2017-07-27 14:08:52 -0400

    Babel SEtup ES2015

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,14 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.js$/ //checks if file ends with .js  babel will apply
+      }
+    ]
   }
 };
 
